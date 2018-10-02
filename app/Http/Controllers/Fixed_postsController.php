a728c10efc1a08ea1f63279e57837619c101eff1
Raquel Balda <stobenetti@gmail.com>
2018-10-01 22:22:37 -0300

    Fixando posts e removendo posts fixos

--- app/Http/Controllers/Fixed_postsController.php
+++ app/Http/Controllers/Fixed_postsController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Fixed_post;
 use Illuminate\Http\Request;
 
-class Fixed_postsController extends Controller
-{
-    //
+class Fixed_postsController extends Controller {
+
+    public function verify($post_id) {
+        $user_id = Auth::id();
+        $result = Fixed_post::where(['user_id' => $user_id, 'post_id' => $post_id])->first();
+        if ($result = null) {
+            $fixed_post = new Fixed_post;
+            $fixed_post->user_id = $user_id;
+            $fixed_post->post_id = $post_id;
+            $fixed_post->save();
+        }
+        else {
+            $result->delete();
+        }
+        return redirect('posts');
+    }
 }
