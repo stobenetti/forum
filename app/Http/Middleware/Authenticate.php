d615198bc29cd96796b8e4a3d9c5b1bb0e960f63
Raquel Balda <stobenetti@gmail.com>
2018-11-11 21:52:55 -0200

    Composer atualizado

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,18 +2,17 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
-class Authenticate extends Middleware
-{
+class Authenticate extends Middleware {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return string
      */
-    protected function redirectTo($request)
-    {
+    protected function redirectTo($request) {
         return route('login');
     }
 }
