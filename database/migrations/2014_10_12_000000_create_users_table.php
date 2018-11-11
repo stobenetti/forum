59197d7f5d7151526e5a8c7d232282c57da1c609
Raquel Balda <stobenetti@gmail.com>
2018-11-11 21:52:45 -0200

    Adaptação de tabela de usuário

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('privilege', 5)->default(1);
             $table->timestamps();
         });
     }
