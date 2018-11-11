00724de3158163e716bc9a5ef3688e87b5641117
Raquel Balda <stobenetti@gmail.com>
2018-11-11 21:52:07 -0200

    Modificações em rotas
    Login por API e aplicação de permissões para moderador

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,19 +17,48 @@ Route::get('/', 'HomeController@index');
 
 Auth::routes();
 
-Route::middleware('auth')->group(function () {
+Route::post('/external_login', function (Request $request) {
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post('http://localhost/bd_api/public/api/login', [
+        'form_params' => [
+            'email' => $request->email,
+            'password' => $request->password,
+        ]
+    ]);
+
+    $res = json_decode((string)$response->getBody(), true);
+    var_dump($res);
+
+    setcookie('access_token', $res['success']['token'], strtotime('+1 year'), '/');
+    setcookie('user_id', $res['user']['id'], strtotime('+1 year'), '/');
+    setcookie('user_privilege', $res['user']['privilege'], strtotime('+1 year'), '/');
+    setcookie('user_name', $res['user']['name'], strtotime('+1 year'), '/');
+    setcookie('user_email', $res['user']['email'], strtotime('+1 year'), '/');
+    return redirect('/posts');
+
+})->name('external_login');
+
+Route::group(['middleware' => 'App\Http\Middleware\Logged_In'], function () {
+//Route::middleware('')->group(function () {
+
+    Route::group(['middleware' => 'App\Http\Middleware\Moderator'], function () {
+        Route::resource('replies', 'RepliesController');
+
+        Route::get('/replies/create/{post_id}', 'RepliesController@create')->name('replies.create');
+
+        Route::get('/replies/delete/{id}', 'RepliesController@delete')->name('replies.delete');
+
+        Route::resource('posts', 'PostsController');
+
+        Route::get('/posts/delete/{id}', 'PostsController@delete')->name('posts.delete');
+    });
+
+    Route::get('/posts', 'PostsController@index')->name('posts.index');
+
+    Route::get('/posts/{id}', 'PostsController@show')->name('posts.show');
 
     Route::get('/home', 'PostsController@index')->name('home');
-
-    Route::resource('posts', 'PostsController');
-
-    Route::get('/posts/delete/{id}', 'PostsController@delete')->name('posts.delete');
-
-    Route::resource('replies', 'RepliesController');
-
-    Route::get('/replies/create/{post_id}', 'RepliesController@create')->name('replies.create');
-
-    Route::get('/replies/delete/{id}', 'RepliesController@delete')->name('replies.delete');
 
     Route::get('/favorites/index/', 'FavoritesController@index')->name('favorites.index');
 
